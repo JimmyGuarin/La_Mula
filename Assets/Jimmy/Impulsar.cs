d719259c4d94155f1a73ab6f925aca53b4b384c4
JimmyGuarin <jimmyguarin43@gmail.com>
2016-07-01 15:50:19 -0500

    Arrojas en movimiento
    Se arrojan las cosas en movimiento

--- Assets/Jimmy/Impulsar.cs
+++ Assets/Jimmy/Impulsar.cs
@@ -8,39 +8,46 @@ public class Impulsar : MonoBehaviour {
     Rigidbody rg;
     public  float velocidad;
     public float distancia;
-    public float t;
+    public float totalTime;
+    public GameObject Canasto;
+    public GameObject Caballo;
+    public int altura = 20;
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start () {
 
+
+        transform.position = new Vector3(Canasto.transform.position.x, Canasto.transform.position.y, transform.position.z);
+        distancia = Mathf.Abs(transform.position.z - Canasto.transform.position.z);
         rg = GetComponent<Rigidbody>();
-
-        float y = (velocidad * 0.6427f) - (Physics.gravity.y * (Mathf.Pow(distancia, 2) / (Mathf.Pow(velocidad, 2) * 0.5868f)));
-
-
-        float x = velocidad * 0.766f;
-
-
-        t = (2 * velocidad * 0.6427f) / -Physics.gravity.y;
-
-        float acel = velocidad / t;
+        float g = Physics.gravity.magnitude;
 
        
-
-        float f = acel * rg.mass;
-
-        //float y = f* 0.6427f;
-        //float x = f * 0.766f;
-        Vector3 v = new Vector3(0, y, x);
-        Debug.Log(v);
-        rg.AddForce(v);
-        
        
-       
+
+
+
+
+        float vertSpeed = Mathf.Sqrt(2 * g *altura);
+        totalTime = 2 * vertSpeed / g;
+
+        float d2 = 30 * totalTime;
+        Debug.Log("d2:" + d2);
+        Debug.Log("distancia" + distancia);
+
+        distancia = Mathf.Abs(distancia - d2);
+
+        Debug.Log("distanciaFinal" + distancia);
+
+        float horSpeed = distancia / totalTime;
+     
+        // calculate the horizontal speed 
+        rg.velocity = new Vector3(0, vertSpeed, horSpeed); // launch the projectile! 
+
     }
-	
-	// Update is called once per frame
-	void Update () {
+
+    // Update is called once per frame
+    void Update () {
 
 
        
