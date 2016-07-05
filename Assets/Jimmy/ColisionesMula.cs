3443160b690e17cf6c764881fda017a27cc506f1
JimmyGuarin <jimmyguarin43@gmail.com>
2016-07-05 16:06:39 -0500

    Generación del suelo

---
+++ Assets/Jimmy/ColisionesMula.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ColisionesMula : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag.Equals("Destructores"))
+        {
+            Debug.Log("Crear");
+            ManejadorSuelo.instancia.GenerarSuelo();
+        }
+    }
+}
