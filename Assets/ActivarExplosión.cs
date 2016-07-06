f6fbd212ebf457cd9ca76d725216cd6b73e95c1f
felipe2323 <felipe23_23@hotmail.com>
2016-07-06 14:14:23 -0500

    Generador

---
+++ Assets/ActivarExplosión.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ActivarExplosión : MonoBehaviour
+{
+
+    public GameObject explosion;
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    public void OnCollisionEnter(Collision collision)
+    {
+        
+        
+        if (!collision.gameObject.name.Equals("Burra"))
+        {
+            Instantiate(explosion, transform.position, transform.rotation);
+           
+            Destroy(gameObject);
+        }
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
